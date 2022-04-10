e415818a673035f65fbf159af66cd4cadd292d0e
Arthur Corona <coronaggp@gmail.com>
2022-04-10 19:01:49 -0300

    f to c, c to f

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('convert') = convert
+document.getElementById('reset') = reset
+
+function convert() {
+    var fahrenheit = document.getElementById('fahrenheit').value
+    var celcius = document.getElementById('celcius').value
+   // var kelvin = document.getElementById('kelvin').value
+
+    if(fahrenheit === '') {
+        fahrenheit = (parseFloat(celcius) * 1.8) + 32
+    }
+    else if(celcius === ''){
+        celcius = (parseFloat(fahrenheit) - 32) / 1.8
+    }
+  //  else if(kelvin === ''){
+
+  //  }
+    
+    document.getElementById("fahrenheit").value = parseFloat(fahrenheit).toFixed(2)
+    document.getElementById("celcius").value = parseFloat(celcius).toFixed(2)
+    //document.getElementById("kelvin").value = parseFloat(kelvin).toFixed(2)
+}
+
+function reset() {
+    
+}
